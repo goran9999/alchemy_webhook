fd5c72d61c4e928527726116a11960f710f59007
goran9999 <gutovicgoran@gmail.com>
2023-07-27 13:25:45 +0200

    move routes below

--- main.ts
+++ main.ts
@@ -8,15 +8,15 @@ const app = Express();
 
 app.use(bodyParser.json());
 
-app.use("/webhook", createWebhook);
-
-app.use("/process", receiveData);
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/webhook", createWebhook);
+
+app.use("/process", receiveData);
 
 app.use((err, req, res) => {
   console.log("Not found");
