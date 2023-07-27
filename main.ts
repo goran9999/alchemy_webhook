99df2457afb09a239322e1a0a81456d56d6702f0
goran9999 <gutovicgoran@gmail.com>
2023-07-27 13:17:44 +0200

    cors

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import Express from "express";
 import bodyParser from "body-parser";
 import { createWebhook } from "./routes/createWebhook";
 import { receiveData } from "./routes/receiveData";
+import express from "express";
+import cors from "cors";
 const app = Express();
 
 app.use(bodyParser.json());
@@ -9,6 +11,12 @@ app.use(bodyParser.json());
 app.get("/webhook", createWebhook);
 
 app.post("/process", receiveData);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(express.json());
+app.use(cors());
 
 app.use((err, req, res) => {
   console.log("Not found");
