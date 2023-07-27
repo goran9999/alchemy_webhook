3c65e8fc737aae3f1c21318ff7339e23019f51e9
goran9999 <gutovicgoran@gmail.com>
2023-07-27 13:32:10 +0200

    logging stringified

--- routes/receiveData.ts
+++ routes/receiveData.ts
@@ -3,7 +3,7 @@ import express from "express";
 export const receiveData = express.Router();
 
 receiveData.post("/receive", async (req, res) => {
-  console.log(req.body.event.activity);
+  console.log(JSON.stringify(req.body.event.activity));
 
   res.status(200).json({ response: req.body });
 });
